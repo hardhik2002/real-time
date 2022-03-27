23a8740fedccab084e6c061031593049a2737844
Hardhik Reddy <96832540+hardhik2002@users.noreply.github.com>
2022-03-28 00:01:40 +0530

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,21 @@
+# programming a digital clock.
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+root=Tk()
+root.title("clock")
+def time():
+   string= strftime("%H:%M:%S %p")
+   label.config(text=string)
+   label.after(1000, time)
+
+label=Label(root,font=("ds-digital",80), background="black",foreground="red" )
+label.pack()
+time()
+mainloop()
+
+
+
+
+
+
